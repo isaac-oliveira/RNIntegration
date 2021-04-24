bc383b95a2bdec4034cc57b814e3588efe813b99
isaac-oliveira <isaac.oliveira@popcode.com.br>
2021-04-24 17:20:03 -0300

    movendo projeto iOS

---
+++ ios/RNIntegration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RNIntegration
+//
+//  Created by Isaac on 24/04/21.
+//
+
+import UIKit
+import React
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
